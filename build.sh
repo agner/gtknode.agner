16ba4a8e5e2fc930c364882e384dac04f12fcd18
Anthony Ramine <nox@dev-extend.eu>
2011-02-05 12:11:17 +0100

    Use `$AGNER_INSTALL_PREFIX` when configuring
    Per agner 0.4.7, variable `$AGNER_INSTALL_PREFIX` is now available during
    build stage, use it to configure gtknode install destination.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 aclocal -I/usr/local/share/aclocal
 autoconf
 automake
-./configure --prefix=`pwd`/rel \
+./configure --prefix="$AGNER_INSTALL_PREFIX" \
             --disable-dependency-tracking \
-            ERLANG_INSTALL_LIB_DIR_gtknode=`pwd`/rel
-make install
+            ERLANG_INSTALL_LIB_DIR_gtknode="$AGNER_INSTALL_PREFIX"
+make
--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cp -vr rel/* "$AGNER_INSTALL_PREFIX"
