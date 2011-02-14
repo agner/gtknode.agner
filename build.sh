976ffbfd8990cf2a6115a53218d3df18defd420f
Yurii Rashkovskii <yrashk@gmail.com>
2011-02-14 01:52:50 -0800

    Make use of /usr/local/share/aclocal conditional
    This way it will work well both on OSX and Linux

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-aclocal -I/usr/local/share/aclocal
+if [ -d /usr/local/share/aclocal ]; then aclocal -I/usr/local/share/aclocal ; else aclocal ; fi
 autoconf
 automake
 ./configure --prefix="$AGNER_INSTALL_PREFIX" \
