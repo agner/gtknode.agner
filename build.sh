46ca940770948aa6a7e6cf72954b4edc80a458c8
Anthony Ramine <nox@dev-extend.eu>
2011-02-14 12:05:21 +0100

    Use nox's fork with real gdk-pixbuf include path fix

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-PIXBUF_IN_CFLAGS=`pkg-config gtk+-2.0 --cflags | grep pixbuf`
-if [ "$PIXBUF_IN_CFLAGS" = "" ]; then git checkout master ; fi
-
 if [ -d /usr/local/share/aclocal ]; then aclocal -I/usr/local/share/aclocal ; else aclocal ; fi
 autoconf
 automake
